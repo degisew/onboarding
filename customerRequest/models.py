3248772cfc73b3f88c894c0c3c2c8d58bbb2c690
Degisew <jegisew21@gmail.com>
2023-12-19 14:33:53 +0300

    Added a field

--- customerRequest/models.py
+++ customerRequest/models.py
@@ -1,4 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group, Permission
 from django.db import models
+from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    is_fraud = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, related_name='customuser_set')
+
+    # Define a unique related_name for the user_permissions field
+    user_permissions = models.ManyToManyField(
+        Permission, related_name='customuser_set'
+    )
+    def __str__(self):
+        return self.username
 
 class CustomerRequest(models.Model):
     service_type = models.CharField(max_length=255)
@@ -8,6 +23,7 @@ class CustomerRequest(models.Model):
     expected_date = models.DateTimeField()
     anything_else = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, default='initiation')
     updated_at = models.DateTimeField(auto_now=True)
 
 
