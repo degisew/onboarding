fb35cf60edcf195eab10a363877cebb48b4f05f7
Degisew <jegisew21@gmail.com>
2023-12-19 14:34:11 +0300

    Added a route

--- customerRequest/urls.py
+++ customerRequest/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import createOnBoardRequest,CRM
+from .views import MoveCardView, createOnBoardRequest,CRM, register, user_login, user_logout
 
 
 urlpatterns = [
     path('request/', createOnBoardRequest.as_view(), name='create-request'),
-    path('crm/', CRM.as_view(), name='crm')
+    path('crm/', CRM.as_view(), name='crm'),
+     path('move_card/', MoveCardView.as_view(), name='move_card'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
