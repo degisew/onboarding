6cba152d008b00c6fcc3b29721cae19cd3c41644
Degisew <jegisew21@gmail.com>
2023-12-19 14:34:38 +0300

    Created a form and new migrations

--- customerRequest/form.py
+++ customerRequest/form.py
@@ -1,5 +1,8 @@
 from django import forms
-from .models import CustomerRequest
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.conf import settings
+from .models import CustomerRequest, CustomUser
 
 class CustomerRequestForm(forms.ModelForm):
 
@@ -16,3 +19,31 @@ class CustomerRequestForm(forms.ModelForm):
                   'request_description', 'expected_date', 'anything_else']
         
         # labels = ["", ""]
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name','last_name', 'username', 'email', 'password1' ,'password2']
+
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password',
+                                                                 'name': 'password',
+                                                                 }))
+    remember_me = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
