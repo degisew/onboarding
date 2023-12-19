730e44a0f9adbf856c2496a584348cb4de024708
Degisew <jegisew21@gmail.com>
2023-12-19 14:31:41 +0300

    configured settings

--- onboard/settings.py
+++ onboard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
 
     #custom apps
     'customerRequest',
-    'CRM'
+    'CRM',
 ]
 
 MIDDLEWARE = [
@@ -122,8 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
--- onboard/urls.py
+++ onboard/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/customer/', include('customerRequest.urls'))
+    path('customer/', include('customerRequest.urls'))
 
 ]
