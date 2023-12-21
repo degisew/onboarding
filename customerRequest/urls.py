788767e4a64aedda21c7c114b4b3a77927341884
Degisew <jegisew21@gmail.com>
2023-12-21 10:56:05 +0300

    adjusted a route

--- customerRequest/urls.py
+++ customerRequest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import MoveCardView, createOnBoardRequest,CRM, CompanyProfile, register, user_login, user_logout
+from .views import MoveCardView, createOnBoardRequest,CRM, CompanyProfile, RegisterView
 
 
 urlpatterns = [
@@ -7,7 +7,9 @@ urlpatterns = [
     path('crm/', CRM.as_view(), name='crm'),
     path('profile/', CompanyProfile.as_view(), name='profile'),
     path('move_card/', MoveCardView.as_view(), name='move_card'),
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
+    path('register/', RegisterView.as_view(), name='users-register'),
+    # path('', home, name='users-home'),
+    # path('register/', register, name='register'),
+    # path('login/', user_login, name='login'),
+    # path('logout/', user_logout, name='logout'),
 ]
