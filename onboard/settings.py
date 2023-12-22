5d0b98339e8ef7809ed26bdebab31370684137b6
Degisew <jegisew21@gmail.com>
2023-12-22 14:43:25 +0300

    Configured database

--- onboard/settings.py
+++ onboard/settings.py
@@ -88,6 +88,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'your_database_name',
+#         'USER': 'root',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
